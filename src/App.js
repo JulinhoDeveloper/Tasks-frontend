e03f4ed8079680eeeb782ad7d2aa2d4aca1f128d
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-09-04 17:58:19 -0300

    criando os reducers de projetos

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import NovaConta from './components/auth/NovaConta';
 import Projetos from './components/projetos/Projetos';
+import ProjetoState from './context/projetos/projetoState';
 
 function App() {
   return(
+    <ProjetoState>
    <Router>
      <Switch>
      <Route exact path="/" component={Login} />
@@ -14,6 +16,7 @@ function App() {
       <Route exact path="/projetos" component={Projetos} />
      </Switch>
    </Router>
+   </ProjetoState>
   );
 }
 
--- src/context/projetos/projetoState.js
+++ src/context/projetos/projetoState.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import projetoContext from './projetoContext';
 import projetoReducer from './projetoReducer';
 
-const projetoState = props => {
+const ProjetoState = props => {
     const initialState = {
-        novoProjeto : false
+        formulario: false
     }
 
     // Dispach para executar as funções
-    
+    const [state, dispatch] = useReducer(projetoReducer, initialState)
+
+     //Serie de funcões para o crud
+     return (
+         <projetoContext.Provider
+         value={{
+             formulario: state.formulario
+         }}
+         >
+             {props.children}
+         </projetoContext.Provider>
+     )
 }
+
+export default ProjetoState;
